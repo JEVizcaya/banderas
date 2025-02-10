dbb3f887e7af9d9154af40e284f7982835450b26
JEVizcaya <jevizcaya@hotmail.com>
2025-02-10 20:14:16 +0100

    banderas con nombre

--- js/banderas.js
+++ js/banderas.js
@@ -1,13 +1,22 @@
-fetch("https://restcountries.com/v3.1/all")
-.then(response=>{
-    $("#cargando").hide();
-    return response.json()})
-.them(data=>{
-    let banderas=document.getElementById("banderas");
-    for (let i=0; i<data.lenght;i++){
-        let img=document.createElement("img");
-        img.src=data[i].flags.png;
-        img.alt=data[i].name.common;
-        banderas.appendChild(img);
-    }
-})
+function cargarBanderas(){
+    fetch("https://restcountries.com/v3.1/all")
+    .then(response =>{
+        $("#cargando").hide();
+        return response.json()})
+    .then(data => {
+        let banderas = document.getElementById("banderas");
+        for (let i = 0; i < data.length; i++) {
+            let img = document.createElement("img");
+            img.src = data[i].flags.png;
+            img.className = "bandera";
+            img.alt = data[i].name.common;
+            banderas.appendChild(img);
+        }
+        $(".bandera").click(function(){ 
+            alert("El país seleccionado es: " + $(this).attr("alt"));
+        })
+    })
+    .catch(error => console.error(error))
+};
+
+setTimeout(cargarBanderas, 5000);
